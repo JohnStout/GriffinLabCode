242a9c8960c4b85e98b0576eb5dd30c94c7a6aad
haileylr <haileylr@udel.edu>
2024-03-21 13:28:33 -0400

    Update empty2nan.m
    Also get index of cells w/ NaN

--- 1. Matlab Pipeline/2. Analysis Pipeline/Libraries/Format Conversions/empty2nan.m
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Libraries/Format Conversions/empty2nan.m
@@ -1,12 +1,15 @@
 %% function that takes a cell array and replaces 
 % empty values with NaN
-function [array] = empty2nan(array)
+function [array,idx2nan] = empty2nan(array)
+idx2nan=zeros(size(array));
     for rowi = 1:size(array,1)
         for coli = 1:size(array,2)
             if isempty(array{rowi,coli})
-                array{rowi,coli} = NaN;       
+                array{rowi,coli} = NaN; 
+                idx2nan(rowi,coli)=1;
             end
         end
     end
+    idx2nan=logical(idx2nan);
 end
 
